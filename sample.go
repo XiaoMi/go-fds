a675b6efd4517df017c8f21f95101d07beece80d
hujianxin <hujianxin@xiaomi.com>
2018-10-10 14:21:41 +0800

    Using environment variables in sample.go

--- sample.go
+++ sample.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/v2tool/galaxy-fds-sdk-go/fds"
 	"github.com/v2tool/galaxy-fds-sdk-go/fds/manager"
 )
 
 func main() {
-	conf, _ := fds.NewClientConfiguration("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-	client := fds.New("xxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxx", conf)
+	conf, _ := fds.NewClientConfiguration(os.Getenv("GO_FDS_TEST_ENDPOINT"))
+	client := fds.New(os.Getenv("GO_FDS_TEST_ACCESS_KEY_ID"), os.Getenv("GO_FDS_TEST_ACCESS_KEY_SECRET"), conf)
 
 	downloader := manager.NewDownloader(client, 1024*1024, 10, true)
 
