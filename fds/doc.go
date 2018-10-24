c902be34b22ae712b6776f007111e06836ccd7fc
hujianxin <hujianxin@xiaomi.com>
2018-10-25 00:27:59 +0800

    删除upload.go
    因为在FDS Server中，绝大多数集群分片上传都是DIRECT模式，这种模式不允许并发分片上传，遂upload.go并不使用。

--- fds/doc.go
+++ fds/doc.go
@@ -1,1 +1,42 @@
+/*
+galaxy-fds-sdk-go是小米云对象存储服务FDS的go语言客户端。
+
+sdk通过封装原始的JSON Restful API，来实现方便地调用。
+
+更多的使用方法，可以通过源码中的fds_test.go文件学到。
+
+Usage:
+
+	package main
+
+	import (
+		"log"
+		"os"
+
+		"github.com/hujianxin/galaxy-fds-sdk-go/fds"
+		"github.com/hujianxin/galaxy-fds-sdk-go/fds/manager"
+	)
+
+	func main() {
+		conf, _ := fds.NewClientConfiguration(os.Getenv("GO_FDS_TEST_ENDPOINT"))
+		client := fds.New(os.Getenv("GO_FDS_TEST_ACCESS_KEY_ID"), os.Getenv("GO_FDS_TEST_ACCESS_KEY_SECRET"), conf)
+
+		downloader := manager.NewDownloader(client, 1024*1024, 10, true)
+
+		request := &manager.DownloadRequest{
+			GetObjectRequest: fds.GetObjectRequest{
+				BucketName: "hellodf",
+				ObjectName: "build.log",
+			},
+			FilePath: "/home/hujianxin/tmp/build.log",
+		}
+		err := downloader.Download(request)
+		if err != nil {
+			log.Fatalln(err)
+		} else {
+			log.Println("Done")
+		}
+	}
+
+*/
 package fds
