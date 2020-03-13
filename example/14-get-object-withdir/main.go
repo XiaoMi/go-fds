712caab43477883c00df562e41c585f7b5732078
hujianxin <hujianxin@xiaomi.com>
2020-03-13 14:32:01 +0800

    Add a get object with dir demo

---
+++ example/14-get-object-withdir/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/XiaoMi/go-fds/fds"
+	"io/ioutil"
+)
+
+func main() {
+	fdsConf, err := fds.NewClientConfiguration(os.Getenv("GO_FDS_TEST_ENDPOINT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fdsClient := fds.New(os.Getenv("GO_FDS_TEST_ACCESS_KEY_ID"), os.Getenv("GO_FDS_TEST_ACCESS_KEY_SECRET"), fdsConf)
+
+	request := &fds.GetObjectRequest{
+		BucketName: "hello",
+		ObjectName: "dir1/dir2/build.log",
+	}
+
+	body, err := fdsClient.GetObject(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer body.Close()
+
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", b)
+}
